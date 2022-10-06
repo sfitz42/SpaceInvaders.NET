ed89978b126acb0a41a1c593e6e96cc918e90e20
sfitz42 <shaunfitz1998@gmail.com>
2022-10-06 01:04:40 +0100

    Implemented SpaceInvaders \ MainMemory classes

---
+++ SpaceInvaders.Core/MainMemory.cs
@@ -0,0 +1,53 @@
+﻿using Intel8080.Emulator;
+using System.IO;
+
+namespace SpaceInvaders.Core
+{
+    public class MainMemory : IMemory
+    {
+        public const int MemorySize = 0x4000;
+        public const int RAMStart = 0x2000;
+        public const int VideoMemoryStart = 0x2400;
+        public const int VideoMemoryEnd = 0x4000;
+
+        private readonly byte[] _memory;
+
+        public byte this[int index]
+        {
+            get
+            {
+                if (index >= MemorySize)
+                    index -= RAMStart;
+
+                return _memory[index];
+            }
+            set
+            {
+                if (index >= RAMStart && index < MemorySize)
+                    _memory[index] = value;
+            }
+        }
+
+        public MainMemory()
+        {
+            _memory = new byte[MemorySize];
+
+            LoadRom("Roms/invaders.h", 0x0000);
+            LoadRom("Roms/invaders.g", 0x0800);
+            LoadRom("Roms/invaders.f", 0x1000);
+            LoadRom("Roms/invaders.e", 0x1800);
+        }
+
+        private void LoadRom(string path, int offset = 0x0000)
+        {
+            using var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+            fs.Read(_memory, offset, (int)fs.Length);
+        }
+
+        public byte[] ReadVRAM()
+        {
+            return _memory[VideoMemoryStart..VideoMemoryEnd];
+        }
+    }
+}
---
+++ SpaceInvaders.Core/SpaceInvaders.cs
@@ -0,0 +1,44 @@
+﻿using Intel8080.Emulator;
+
+namespace SpaceInvaders.Core
+{
+    public class SpaceInvaders
+    {
+        public enum Interrupt
+        {
+            MidFrame = 0xCF,
+            EndFrame = 0xD7
+        }
+
+        public const int ScreenWidth = 256;
+        public const int ScreenHeight = 224;
+        public const int FramesPerSecond = 60;
+
+        public CPU Cpu { get; }
+
+        public MainMemory Memory { get; }
+
+        private Interrupt _nextInterrupt = Interrupt.MidFrame;
+
+        public SpaceInvaders()
+        {
+            Memory = new MainMemory();
+            Cpu = new CPU(Memory);
+        }
+
+        public void GenerateInterrupt()
+        {
+            Cpu.RaiseInterrupt((byte) _nextInterrupt);
+
+            if (_nextInterrupt == Interrupt.MidFrame)
+                _nextInterrupt = Interrupt.EndFrame;
+            else if (_nextInterrupt == Interrupt.EndFrame)
+                _nextInterrupt = Interrupt.MidFrame;
+        }
+
+        public void Run()
+        {
+
+        }
+    }
+}

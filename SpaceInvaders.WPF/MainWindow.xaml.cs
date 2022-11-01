7e5874ae878d7ece2f06d80646daafdb0705d5d0
sfitz42 <shaunfitz1998@gmail.com>
2022-11-01 00:41:57 +0000

    Added window scaling factor

--- SpaceInvaders.WPF/MainWindow.xaml.cs
+++ SpaceInvaders.WPF/MainWindow.xaml.cs
@@ -11,6 +11,9 @@ namespace SpaceInvaders.WPF
     public partial class MainWindow : Window
     {
         private const double ScreenRefreshRate = 16.666667;
+        private const int DisplayScale = 2;
+        private const int DisplayWidth = ArcadeMachine.ScreenHeight * DisplayScale;
+        private const int DisplayHeight = ArcadeMachine.ScreenWidth * DisplayScale;
 
         private readonly ArcadeMachine _arcadeMachine = new();
         private readonly DispatcherTimer _timer;
@@ -35,6 +38,11 @@ namespace SpaceInvaders.WPF
 
         public void OnLoad(object sender, RoutedEventArgs e)
         {
+            InvadersDisplay.Width = DisplayWidth;
+            InvadersDisplay.Height = DisplayHeight;
+            MainView.Width = DisplayWidth;
+            MainView.Height = DisplayHeight;
+
             /* All elements will default to 0 (black) */
             var pixels = new int[ArcadeMachine.ScreenWidth * ArcadeMachine.ScreenHeight];
 

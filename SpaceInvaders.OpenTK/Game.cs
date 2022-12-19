d0041e3f4d296cae66c36c3e6221e384234e9f22
sfitz42 <shaunfitz1998@gmail.com>
2022-12-19 23:43:24 +0000

    Added OpenTK default window

---
+++ SpaceInvaders.OpenTK/Game.cs
@@ -0,0 +1,38 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace SpaceInvaders.OpenTK
+{
+    public class Game : GameWindow
+    {
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+                Close();
+
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            SwapBuffers();
+        }
+    }
+}
--- SpaceInvaders.OpenTK/Program.cs
+++ SpaceInvaders.OpenTK/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using SpaceInvaders.Core;
+
+namespace SpaceInvaders.OpenTK
+{
+    class Program
+    {
+        private const int DisplayWidth = ArcadeMachine.ScreenHeight;
+        private const int DisplayHeight = ArcadeMachine.ScreenWidth;
+
+        static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(DisplayWidth, DisplayHeight),
+                Title = "SpaceInvaders.NET",
+                WindowBorder = WindowBorder.Fixed,
+                Flags = ContextFlags.ForwardCompatible
+            };
+
+            using (Game game = new(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                game.Run();
+            }
+        }
+    }
+}

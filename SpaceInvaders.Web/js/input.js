a12bbd045bf5f824a2c0c74b5563cdb9372581b9
sfitz42 <shaunfitz1998@gmail.com>
2023-03-06 13:30:12 +0000

    Added arcade style buttons

--- SpaceInvaders.Web/js/input.js
+++ SpaceInvaders.Web/js/input.js
@@ -1,13 +1,37 @@
 import { methods } from './main.js'
 
-window.addEventListener("keydown", (event) => {
-    methods.SetKey(event.key.replace("Arrow", ""), true);
+const keyMap = {
+    "LEFT": "Left",
+    "RIGHT": "Right",
+    " ": "Fire",
+    "ENTER": "1UP",
+    "BACKSPACE": "2UP",
+    "C": "Credit"
+};
+
+function handleKey(event, pressed) {
+    const key = event.key.replace("Arrow", "").toUpperCase();
+
+    methods.SetKey(keyMap[key], pressed);
 
     event.preventDefault();
-});
+}
 
-window.addEventListener("keyup", (event) => {
-    methods.SetKey(event.key.replace("Arrow", ""), false);
+function registerButton(id, mappedAction) {
+    const button = document.getElementById(id);
 
-    event.preventDefault();
-});
+    button.addEventListener("mousedown", () => methods.SetKey(mappedAction, true));
+    button.addEventListener("mouseup", () => methods.SetKey(mappedAction, false));
+    button.addEventListener("touchstart", () => methods.SetKey(mappedAction, true));
+    button.addEventListener("touchend", () => methods.SetKey(mappedAction, false));
+}
+
+window.addEventListener("keydown", (event) => handleKey(event, true));
+window.addEventListener("keyup", (event) => handleKey(event, false));
+
+registerButton('left', 'Left');
+registerButton('right', 'Right');
+registerButton('fire', 'Fire');
+registerButton('1up', '1UP');
+registerButton('2up', '2UP');
+registerButton('credit', 'Credit');
